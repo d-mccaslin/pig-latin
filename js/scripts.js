aab6d567ec1b22152c667b4a2c71621bffe8d594
David McCaslin <davidmccaslin94@gmail.com>
2020-08-19 16:18:48 -0700

    update business logic functions, fix several issues preventing two tests (rules 2 & 3) from passing, refactor business logic functions
    Co-authored-by: David Cole <davidtcole@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,59 +3,38 @@ $(document).ready(function(){
   $("#input").submit(function(event){
     event.preventDefault();
     const userInput = $("#userPhrase").val();
-    console.log(vowelSearch(userInput));
+    console.log(pigLatin(userInput));
+    //console.log(pigLatin(userInput));
   });
 
 });
 
+function pigLatinWithRegex(string) {
+  //return newString = string.replace(/[aeiou]/i,"x");
+}
+
 //Business Logic
-function vowelSearch(string) {
+function pigLatin(string) {
   const vowels = ["a", "e", "i", "o", "u"];
   string = string.toLowerCase();
 
   if (vowels.indexOf(string[0]) > -1) {
     return string = string + "way";
+  } else if (string[0].includes("q") && string[1].includes("u")) {
+    let quCons = string.slice(0, 2);
+    let newString = string.slice(2, string.length);
+    string = newString + quCons + "ay";
+    return string;
   } else {
       for (let i = 0; i < string.length; i++) {
-        if (vowels.indexOf(string[i]) > -1) {
+          if (vowels.indexOf(string[i]) > -1) {
           let consonants = string.slice(0, i);
           let newString = string.slice(i, string.length);
           string = newString + consonants + "ay";
           break;
           }
-        }
+      }
       return string;
-      }
-  }
-
-
-            //if ((string.charAt(i) === "u") && (string.charAt(i-1) === "q")) {
-            //else {
-            //   let consonants = string.slice(0, i);
-            //   let newString = string.slice(i, string.length);
-            //   string = newString + consonants + "ay";
-            //   break;
-
-
-
-
-/*
-function consonantSearch(string) {
-  const vowels = ["a", "e", "i", "o", "u"];
-  const consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
-  string = string.toLowerCase();
-
-  let target;
-  if (consonants.indexOf(string[0]) > -1) {
-    for (let i = 0; i < string.length; i++) {
-      let letter = string[i];
-      if (consonants.includes(letter)) {
-        target = target + letter;
-      } else if ("not consonant") {
-        break;
-      }
-    return string = string + target + "ay";
-  } */
-
-
+    }
+}
 
