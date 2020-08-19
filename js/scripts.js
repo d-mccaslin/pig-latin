62c378cebad4e57b08902d52582e5cd8cb3e8475
David McCaslin <davidmccaslin94@gmail.com>
2020-08-19 14:08:34 -0700

    add/update business logic functions to test test cases
    Co-authored-by: David Cole <davidtcole@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,41 @@
+//UI Logic
 $(document).ready(function(){
+  $("#input").submit(function(event){
+    event.preventDefault();
+    const userInput = $("#userPhrase").val();
+  });
 
 });
+
+//Business Logic
+function vowelSearch(string) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  string = string.toLowerCase();
+
+  if (vowels.indexOf(string[0]) > -1) {
+    return string = string + "way";
+  } else {
+    
+  }
+}
+
+function consonantSearch(string) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  const consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
+  string = string.toLowerCase();
+
+  let target;
+  if (consonants.indexOf(string[0]) > -1) {
+    for (let i = 0; i < string.length; i++) {
+      let letter = string[i];
+      if (consonants.includes(letter)) {
+        target = target + letter;
+      } else if ("not consonant") {
+        break;
+      }
+    return string = string + target + "ay";
+  }
+
+
+}
+
